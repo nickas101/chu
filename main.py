d198f2257b71434054b3fe8c0336fabbbf342f7c
nickas101 <nikolai.astashkin@rakon.com>
2020-04-06 09:34:52 +1200

    waitress is added

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from app import app
+from waitress import serve
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    #app.run(debug=True)
+    serve(app, host='172.20.7.226', port=8080)
     #app.run('0.0.0.0', 8080)
     # app.run('0.0.0.0', 80)
