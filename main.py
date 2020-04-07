f984c0f3a81d489266d7868b532c340b93e487a2
nickas101 <nikolai.astashkin@rakon.com>
2020-04-08 11:00:39 +1200

    external ip

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from waitress import serve
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    #serve(app, host='172.20.7.226', port=8080)
+    #app.run(debug=True)
+    serve(app, host='172.20.7.226', port=8080)
     #app.run('0.0.0.0', 8080)
     # app.run('0.0.0.0', 80)
