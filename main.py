449ee43ded78bac1907b7cd103c4f50afc6e3603
nick <nickas101@gmail.com>
2020-04-25 21:28:16 +1200

    run local with waitress

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from waitress import serve
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    serve(app, host='127.0.0.1', port=5000)
     #serve(app, host='172.20.7.226', port=8080)
     #app.run('0.0.0.0', 8080)
     # app.run('0.0.0.0', 80)
