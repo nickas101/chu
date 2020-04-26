ce443d275a379928bbd4bcddae8122a6c193dbd3
nick <nickas101@gmail.com>
2020-04-26 13:43:59 +1200

    requirements.txt is renewed

--- main.py
+++ main.py
@@ -18,5 +18,3 @@ if __name__ == "__main__":
 
     # serve(app, host='127.0.0.1', port=5000)
     #serve(app, host='172.20.7.226', port=8080)
-    #app.run('0.0.0.0', 8080)
-    # app.run('0.0.0.0', 80)
