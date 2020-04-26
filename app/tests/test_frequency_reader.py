40f5043a73a6f5447a54666bf7b95dc999aee060
nick <nickas101@gmail.com>
2020-04-26 23:25:44 +1200

    first test is added

---
+++ app/tests/test_frequency_reader.py
@@ -0,0 +1,8 @@
+import pytest
+
+from ..lib import frequency_reader
+
+
+def test_frequency_reader():
+    # assert 1==1
+    assert frequency_reader.read_frequency("\t_define nominalFreq-30.720;", '_define nominalFreq-') == '30.72'
