28c49badf054d78fc7e7c53ece20d885e647324a
nick <nickas101@gmail.com>
2020-05-08 12:10:50 +1200

    Bag with converting NaN from the solver is fixed

--- app/lib/solver_table_converter.py
+++ app/lib/solver_table_converter.py
@@ -29,7 +29,8 @@ def convert_short(input_table):
     for column in input_table.columns:
         if column != 'DUT' and column != 'pos':
             column_name = str(column)
-            output_table[column_name] = input_table[column].astype(int)
+            output_table[column_name] = input_table[column]
+            output_table[column_name] = output_table[output_table[column_name].notna()][column_name].astype(int)
 
 
     return output_table
