371ebf2647eee27464fbaeb4a5883f2014a2f954
nick <nickas101@gmail.com>
2020-05-10 10:54:48 +1200

    tests are ajusted to new solver version

--- app/lib/solver_table_converter.py
+++ app/lib/solver_table_converter.py
@@ -3,7 +3,6 @@ from natsort import natsorted, ns
 
 
 def convert(input_table):
-    bad_units = ''
     output_table = pd.DataFrame()
 
     output_table['DUT'] = input_table['DUT'].astype(int)
@@ -20,9 +19,8 @@ def convert(input_table):
             output_table_short[column_name_short] = input_table[column].astype(pd.Int64Dtype())
 
     bad_units_df = output_table[output_table.isna().any(axis='columns')]
-    bad_units_df.sort_values(['pos'], ascending=[True], inplace=True)
 
     bad_units_list = natsorted(bad_units_df['pos'].unique().tolist())
-    bad_units = bad_units + " " + ", ".join(str(int(x)) for x in bad_units_list)
+    bad_units = ', '.join(str(int(x)) for x in bad_units_list)
 
     return output_table_short, output_table, bad_units
--- tests/test_solver_wrapper.py
+++ tests/test_solver_wrapper.py
@@ -4,33 +4,40 @@ import pandas as pd
 from app.lib import solver_wrapper
 
 
-@pytest.mark.parametrize('result', [True])
-def test_solver_wrapper_success(dataframe_for_solver, result):
-    success_solver, message_solver, solver_output, bad_units = solver_wrapper.wrap(dataframe_for_solver)
+@pytest.mark.parametrize('cut_number, result', [(7, True)])
+def test_solver_wrapper_success(dataframe_for_solver, cut_number, result):
+    success, message, solver_output, prediction, bad_units = solver_wrapper.wrap(dataframe_for_solver, cut_number)
 
-    assert success_solver == result
-
-
-@pytest.mark.parametrize('result', [''])
-def test_solver_wrapper_text(dataframe_for_solver, result):
-    success_solver, message_solver, solver_output, bad_units = solver_wrapper.wrap(dataframe_for_solver)
-
-    assert message_solver == result
+    assert success == result
 
 
-@pytest.mark.parametrize('result', [False])
-def test_solver_wrapper_failure(result):
+@pytest.mark.parametrize('cut_number, result', [(7, False)])
+def test_solver_wrapper_failure(cut_number, result):
     dataframe_for_solver = pd.DataFrame()
 
-    success_solver, message_solver, solver_output, bad_units = solver_wrapper.wrap(dataframe_for_solver)
+    success, message, solver_output, prediction, bad_units = solver_wrapper.wrap(dataframe_for_solver, cut_number)
 
-    assert success_solver == result
+    assert success == result
 
 
-@pytest.mark.parametrize('result', [' *** Problem with solver calculations'])
-def test_solver_wrapper_text_failure(result):
+@pytest.mark.parametrize('cut_number, result', [(7, '')])
+def test_solver_wrapper_text_success(dataframe_for_solver, cut_number, result):
+    success, message, solver_output, prediction, bad_units = solver_wrapper.wrap(dataframe_for_solver, cut_number)
+
+    assert message == result
+
+
+@pytest.mark.parametrize('cut_number, result', [(7, ' *** Problem with solver calculations')])
+def test_solver_wrapper_text_failure(cut_number, result):
     dataframe_for_solver = pd.DataFrame()
 
-    success_solver, message_solver, solver_output, bad_units = solver_wrapper.wrap(dataframe_for_solver)
+    success, message, solver_output, prediction, bad_units = solver_wrapper.wrap(dataframe_for_solver, cut_number)
 
-    assert message_solver == result
+    assert message == result
+
+
+@pytest.mark.parametrize('cut_number, result', [(7, '3, 6, 10')])
+def test_solver_wrapper_bad_units(dataframe_for_solver, cut_number, result):
+    success, message, solver_output, prediction, bad_units = solver_wrapper.wrap(dataframe_for_solver, cut_number)
+
+    assert bad_units == result
