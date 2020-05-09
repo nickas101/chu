57ff143d5b92a81b8416939c93c41ceb43717a80
nick <nickas101@gmail.com>
2020-05-09 22:32:38 +1200

    solver table converter is improved

--- app/lib/solver_table_converter.py
+++ app/lib/solver_table_converter.py
@@ -3,52 +3,26 @@ from natsort import natsorted, ns
 
 
 def convert(input_table):
-
+    bad_units = ''
     output_table = pd.DataFrame()
 
     output_table['DUT'] = input_table['DUT'].astype(int)
     output_table['DUT'] = output_table['DUT'].astype(str)
     output_table['pos'] = input_table['pos'].astype(int)
+
+    output_table_short = output_table.copy()
 
     for column in input_table.columns:
         if column != 'DUT' and column != 'pos':
             column_name = 'Table-' + str(column)
-            output_table[column_name] = input_table[column]
-            output_table[column_name] = output_table[output_table[column_name].notna()][column_name].astype(int)
+            column_name_short = str(column)
+            output_table[column_name] = input_table[column].astype(pd.Int64Dtype())
+            output_table_short[column_name_short] = input_table[column].astype(pd.Int64Dtype())
 
-    bad_units = find_bad_units(output_table)
+    bad_units_df = output_table[output_table.isna().any(axis='columns')]
+    bad_units_df.sort_values(['pos'], ascending=[True], inplace=True)
 
-    return output_table, bad_units
+    bad_units_list = natsorted(bad_units_df['pos'].unique().tolist())
+    bad_units = bad_units + " " + ", ".join(str(int(x)) for x in bad_units_list)
 
-
-def convert_short(input_table):
-
-    output_table = pd.DataFrame()
-
-    output_table['DUT'] = input_table['DUT'].astype(int)
-    output_table['DUT'] = output_table['DUT'].astype(str)
-    output_table['pos'] = input_table['pos'].astype(int)
-
-    for column in input_table.columns:
-        if column != 'DUT' and column != 'pos':
-            column_name = str(column)
-            output_table[column_name] = input_table[column]
-            output_table[column_name] = output_table[output_table[column_name].notna()][column_name].astype(int)
-
-    bad_units = find_bad_units(output_table)
-
-    return output_table, bad_units
-
-
-def find_bad_units(table):
-    bad_units = ''
-    # print(table)
-
-    # bad_units_df = table[table.isna().any(axis='columns')]['pos']
-    # # bad_units_df.sort_values(['pos'], ascending=[True], inplace=True)
-    # print(bad_units_df)
-    #
-    # bad_units_list = natsorted(bad_units_df['pos'].unique().tolist())
-    # bad_units = bad_units + " " + ", ".join(str(int(x)) for x in bad_units_list)
-
-    return bad_units
+    return output_table_short, output_table, bad_units
--- app/lib/solver_wrapper.py
+++ app/lib/solver_wrapper.py
@@ -8,13 +8,19 @@ def wrap(result_cutted, cut_number):
     success = True
     message = ''
     solver_output = pd.DataFrame()
+    solver_output_short = pd.DataFrame()
+    prediction = pd.DataFrame()
     bad_units = ''
 
     try:
         solver, prediction = comp_solver.solve(result_cutted, cut_number)
-        solver_output, bad_units = solver_table_converter.convert_short(solver)
+        solver_output_short, solver_output, bad_units = solver_table_converter.convert(solver)
+        # print(solver)
+        # print(solver.info())
+        # print(solver_output)
+        # print(solver_output.info())
     except:
         message = " *** Problem with solver calculations"
         success = False
 
-    return success, message, solver_output, prediction, bad_units
+    return success, message, solver_output_short, prediction, bad_units
